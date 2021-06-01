aed46c3a83bfb933cdc13c16ff224e7afe1e9310
adrielk <theadrielkim@gmail.com>
2021-06-01 16:47:47 -0400

    class directory ui complete

--- src/components/ClassDirectory.js
+++ src/components/ClassDirectory.js
@@ -13,6 +13,9 @@ import {
 import SearchIcon from '@material-ui/icons/Search'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
+const grey = "#E5E5E5";
+const blue = "#004981";
+
 //To do extra:
 //Autocomplete search
 //Idea: Scrape uva profs
@@ -71,7 +74,7 @@ const subjects = [
     "Art", 
     "Geography"
 ]
-//make grade level(s) a list
+//make grade level(s) a list. There can be multiple classes (with different times or locations)
 const classData=[
     {title:"World History", subject:"History", gradeLevel:1},
     {title:"US History",subject:"History", gradeLevel:2},
@@ -80,20 +83,40 @@ const classData=[
     {title:"Basic Arithmetic",subject:"Math", gradeLevel:2},
     {title:"Advanced Math", subject:"Math", gradeLevel:6},
     {title:"Art 101", subject:"Art", gradeLevel:2},
+    {title:"Art 201", subject:"Art", gradeLevel:3},
+    {title:"Art 301", subject:"Art", gradeLevel:4},
+    {title:"Art 401", subject:"Art", gradeLevel:5},
     {title:"Physical Health", subject:"Health", gradeLevel:6},
+    {title:"Soccer", subject:"Health", gradeLevel:6},
+    {title:"Sex Ed", subject:"Health", gradeLevel:6},
+    {title:"Nutrition", subject:"Health", gradeLevel:6},
     {title:"World Geography", subject:"Geography", gradeLevel:3},
-
+    {title:"Chemistry", subject:"Science", gradeLevel:3},
+    {title:"Physics", subject:"Science", gradeLevel:3},
+    {title:"Physics", subject:"Science", gradeLevel:3},
+    {title:"Physics", subject:"Science", gradeLevel:3},
+    {title:"Physics", subject:"Science", gradeLevel:3},
+    {title:"Physics", subject:"Science", gradeLevel:3},
+    {title:"English 101", subject:"English", gradeLevel:3},
+    {title:"English 201", subject:"English", gradeLevel:2},
+    {title:"English 301", subject:"English", gradeLevel:1},
+    {title:"English 401", subject:"English", gradeLevel:5},
+    {title:"English 501", subject:"English", gradeLevel:6},
+    {title:"English 601", subject:"English", gradeLevel:2},
+    {title:"English 701", subject:"English", gradeLevel:3},
+    {title:"English 801", subject:"English", gradeLevel:4},
+    {title:"World Geography 2", subject:"Geography", gradeLevel:3},
 ]
 
 function ClassDirectory() {
     const buttonGroupStyle={
-        backgroundColor:"#004981",
+        backgroundColor:blue,
         borderWidth:"0px",
         fontWeight:"bold",
         color:"white",
     }
     const InputStyle={
-        backgroundColor:"#E5E5E5",
+        backgroundColor:grey,
         borderRadius:"10px",
         padding:"5px",
     }
@@ -112,15 +135,10 @@ function ClassDirectory() {
                     </ButtonGroup>
                 </div>
                 <div className="td__table">
-                    {/* <div className="td__table__columns color-theme">
-                        <div className="column-label">Name</div>
-                        <div className="column-label">Subject(s)</div>
-                        <div className="column-label">Average GPA</div>
-                        <div className="spacer"/>
-                    </div> */}
                     {
                         subjects.map((subject)=>{
-                            <ClassAccordion subject={subject}/>
+                            const classes = classData.filter((aClass) => aClass.subject===subject)
+                            return(<ClassAccordion {...{subject, classes}}/>)
                         })
                     }
                 </div>
@@ -128,11 +146,19 @@ function ClassDirectory() {
     )
 }
 
-function ClassAccordion({subject}){
+function ClassAccordion({subject, classes}){
     const accordionStyle={
         width:"100%",
         paddingRight:"20px",//based off td__table__columns
     }
+    const classButtonStyle={width:"100%", 
+                            padding:0, 
+                            backgroundColor: grey,
+                            color:"black",
+                            padding: "2px",
+                            paddingLeft:"10px",
+                            borderRadius: "5px",
+                            justifyContent: "flex-start"                        }
     return(
         <Accordion style={accordionStyle}>
             <AccordionSummary
@@ -146,17 +172,19 @@ function ClassAccordion({subject}){
                 </div>
             </AccordionSummary>
             <AccordionDetails>
-                <div className="accordion-details">
-                    Some extra info in a flexbox or grid...
+                <div className="class-container">
+                    {classes &&
+                        classes.map((aClass)=>{
+                            return(<div className="class-title-container">
+                                        <div className="class-title-button-container">
+                                            <Button style={classButtonStyle}>{aClass.title}</Button>   
+                                        </div>
+                                   </div>)
+                                })
+                    }
                 </div>
             </AccordionDetails>
         </Accordion>
-    )
-}
-
-function ProfilePic({teacher}){
-    return(
-        <img className="profile-pic" src="http://placehold.it/100x100"/>
     )
 }
 
