f563b9553aae9e1dc6ccdd7e5e6fcca147bcb5d9
Brandon Bremer <beb8nj@virginia.edu>
2021-06-01 14:12:42 -0400

    Cleaned Up Some stuff

--- jefferson-elementary/src/App.js
+++ jefferson-elementary/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import CalendarApp from "./Calendar.js";
+import CalendarApp from "./components/Calendar.js";
 function App() {
   return (
     <div style={{ textAlign: "center" }}>
