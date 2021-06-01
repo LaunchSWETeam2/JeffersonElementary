740454e362caa8ce4d1d0445b0457a2a21cfe10d
adrielk <theadrielkim@gmail.com>
2021-06-01 10:42:08 -0400

    dashboard ui

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import './css/index.css'
+
+//testing
+import Dashboard from './components/Dashboard'
+
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Dashboard/>
     </div>
   );
 }
