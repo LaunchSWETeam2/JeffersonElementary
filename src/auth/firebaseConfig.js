f068ed7a81ba006ba691e2199833f03d495b3567
noblecp <60997156+noblecp@users.noreply.github.com>
2021-06-04 10:53:43 -0400

    Create firebaseConfig.js

---
+++ src/auth/firebaseConfig.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAFFw6ELbCsPo08TBnVJ64n0S27lLvTOBk",
+  authDomain: "school-dashboard-team2-ca89e.firebaseapp.com",
+  projectId: "school-dashboard-team2-ca89e",
+  storageBucket: "school-dashboard-team2-ca89e.appspot.com",
+  messagingSenderId: "994107196048",
+  appId: "1:994107196048:web:07033e2f17f15df4078e15",
+  measurementId: "G-JT4MCGNNH7",
+};
+module.exports = firebaseConfig;
