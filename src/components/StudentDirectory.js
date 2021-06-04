0468430829251231f56d7af208860f0e9a599cea
Justin <jhj4np@virginia.edu>
2021-06-03 23:44:39 -0400

    student edit

--- src/components/StudentDirectory.js
+++ src/components/StudentDirectory.js
@@ -1,19 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import StudentDialog from './StudentDialog'
+import StudentDialog from './StudentDialog';
+import StudentAccordion from './StudentAccordion';
 import '../css/teacher-directory-style.css';
 import {
     Button,
     ButtonGroup,
     Input,
-    Accordion,
-    AccordionSummary,
-    AccordionDetails,
-    Checkbox,
-    FormControlLabel,
     Snackbar 
 } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import axios from 'axios';
 //To do extra:
@@ -27,16 +22,22 @@ function StudentDirectory({allFaces}) {
     const [studentData, setStudentData] = useState([])
     const [openForm, setOpenForm] = useState(false)
 
+    //multiple select states
     const [selectMode, setSelectMode] = useState(false)
     const [selected, setSelected] = useState([])
+
+    //update student states
+    const [isUpdating, setIsUpdating] = useState(false)
+    const [studentEdit, setStudentEdit] = useState(null)
 
     //yummmy snacc bar
     const [snackState, setSnackState] = useState({
         snackOpen: false,
         vertical: 'top',
         horizontal: 'center',
+        message:"Message here"
     });
-    const { vertical, horizontal, snackOpen } = snackState;
+    const { vertical, horizontal, snackOpen, message } = snackState;
 
     const baseButtonStyle={
         backgroundColor:darkBlue,
@@ -68,18 +69,27 @@ function StudentDirectory({allFaces}) {
             .catch(err=>{
                 console.log("Fetch Student Error: ", err)
             })
+
     }
 
     const handleDelete = () =>{
-        selected.forEach((id)=>{
+        selected.forEach((id, index)=>{
             const url = new URL("http://localhost:8080/students/delete");
             url.searchParams.append('id', id)
             axios.delete(url)
+                .then(response=>{
+                    if(index === selected.length-1){
+                        setStudentData(studentData.filter(data=>selected.includes(data.id) === false))
+                        handleSnackOpen("Deletion successful!")
+                        setSelected([])
+                        setSelectMode(false)
+                    }
+                })
+                .catch(err=>{
+                    console.log("Deletion error: ", err)
+                    handleSnackOpen("Deletion failed")
+                })
         })
-        setStudentData(studentData.filter(data=>selected.includes(data.id) === false))
-        setSnackState({...snackState, snackOpen:true });
-        setSelected([])
-        setSelectMode(false)
     }
 
     const handleOpenForm = () =>{
@@ -88,6 +98,11 @@ function StudentDirectory({allFaces}) {
     
     const handleCloseForm = () =>{
         setOpenForm(false)
+        handleEditClose()//for reseting edit mode
+    }
+
+    const handleSnackOpen = (message) =>{
+        setSnackState({...snackState,  message:message, snackOpen:true });
     }
 
     const handleSnackClose = () =>{
@@ -98,22 +113,35 @@ function StudentDirectory({allFaces}) {
         setSelectMode(!selectMode)
     }
 
+    const handleEditOpen = (student) =>{
+        setOpenForm(true)
+        setIsUpdating(true)
+        setStudentEdit(student)
+    }
+    const handleEditClose = () =>{
+        setIsUpdating(false)
+        setStudentEdit(null)
+    }
+
+
     return (
-        <div className="student-directory">
+        <div className="teacher-directory">
             <h2 className="td__header">Student Directory</h2>
                 <div className="td__controller">
                     <div className="td__controller__search">
-                        <Input style={InputStyle} disableUnderline={true} id="student-search-input"  placeholder="Search Name"/>
+                        <Input style={InputStyle} disableUnderline={true} id="teacher-search-input"  placeholder="Search Name"/>
                         <Button><SearchIcon/></Button>
                     </div>
-                    <ButtonGroup variant="contained" aria-label="student-directory-controls">
+                    <ButtonGroup variant="contained" aria-label="teacher-directory-controls">
                         <Button onClick={handleOpenForm} style={baseButtonStyle}>Add</Button>
 
                         <StudentDialog 
                             open={openForm} 
                             handleClose={handleCloseForm} 
-                            //allFaces={allFaces} 
+                            allFaces={allFaces} 
                             setStudentData={setStudentData}
+                            handleSnackOpen={handleSnackOpen}
+                            studentEdit={studentEdit}
                         />
 
                         <Button onClick={toggleSelect} style={selectButtonStyle}>Select</Button>
@@ -123,103 +151,26 @@ function StudentDirectory({allFaces}) {
                         anchorOrigin={{ vertical, horizontal }}
                         open={snackOpen}
                         onClose={handleSnackClose}
-                        message={"Deletion successful!"}
+                        message={message}
                         key={vertical + horizontal}
                     />
                 </div>
                 <div className="td__table">
                     <div className="td__table__columns color-theme">
                         <div className="column-label">Name</div>
-                        <div className="column-label">Grade Level</div>
+                        <div className="column-label">Subject(s)</div>
                         <div className="spacer"/>
                     </div>
                     {
                         studentData.map((student)=>{
                             return(
                                 <StudentAccordion key={student.id}
-                                {...{student, selectMode, selected, setSelected}}/>             
+                                {...{student, selectMode, selected, setSelected, handleEditOpen}}/>             
                             )
                         })
                     }
                 </div>
         </div>
-    )
-}
-
-
-function StudentAccordion({student, selectMode, selected, setSelected}){
-    const [isSelected, setIsSelected] = useState(false)
-    const accordionStyle={
-        width:"100%",
-        paddingRight:"20px",//based off td__table__columns
-    }
-
-    useEffect(()=>{
-        getSelected()
-    }, [])
-
-    const getSelected = ()=>{
-        if(selected.includes(student.id)){
-            setIsSelected(true)
-        }
-        else{
-            setIsSelected(false)
-        }
-    }
-
-    const handleSelect = (e) =>{
-        e.stopPropagation()
-        const checked = e.target.checked;
-        const id = e.target.name;
-        setIsSelected(checked)
-        if(checked === true){
-            setSelected(arr => arr.concat([id]))
-            setIsSelected(true)
-        }
-        else{
-           setSelected(arr => arr.filter(elem=> elem !==id))
-           setIsSelected(false) 
-        }
- 
-    }
-    return(
-        <Accordion style={accordionStyle}>
-            <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-                style={{padding:"0px"}}
-            >
-                <div className="td__table__columns">
-              
-                    <div className="column-label">
-                        {selectMode === true &&
-                            <FormControlLabel
-                                aria-label="Acknowledge"
-                                onClick={handleSelect}
-                                onFocus={(event) => event.stopPropagation()}
-                                control={<Checkbox checked={isSelected} name={student.id} style={{padding:"0px", paddingLeft:"5px"}} />}
-                            />
-                        }
-                        {student.name}
-                    </div>
-                    <div className="column-label">{student.gradeLevel}</div>
-                </div>
-            </AccordionSummary>
-            <AccordionDetails>
-                <div className="accordion-container">
-                    {/*<img className="profile-pic" src={student.image} />*/}
-                    <div className="accordion-details">
-                        <h4>Age: {student.age}</h4>
-                        <h4>Gender: {student.gender}</h4>
-                        {/* <h4>Rating: {teacher.rating}</h4> */}
-                        <h4>Email: {student.contact.email}</h4>
-                        <h4>Phone: {student.contact.phone}</h4>
-                        <Button style={{width:"100%",height:"25px"}}variant="contained">Edit</Button>
-                    </div>
-                </div>
-            </AccordionDetails>
-        </Accordion>
     )
 }
 
